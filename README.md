a7a7bea751a4165ec8640fb8b6062b53b18550b2
付志强 <zhiqiangfu6@gmail.com>
2022-12-06 18:43:32 +0800

    chore: auto issues

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # github-action-auto-issues
+
 使用 Github Actions 来完成自动创建 issues 任务
+
+# V1
+
+每日自动创建 issues
+
+自动执行
+使用 js 来创建 issues
---
+++ index.js
@@ -0,0 +1,29 @@
+const { Octokit } = require("octokit");
+const core = require("@actions/core");
+const dayjs = require("dayjs");
+
+// nodejs12
+
+const token = core.getInput("token");
+const octokit = new Octokit({
+    auth: token,
+});
+
+// octokit api create issue
+
+octokit.rest.issues.create({
+    owner: "sunny-117",
+    repo: "github-action-auto-issues",
+    title: getTitle(),
+    body: getBody(),
+});
+
+// YYYY-MM-DD
+
+function getTitle() {
+    return dayjs().format("YYYY-MM-DD");
+}
+
+function getBody() {
+    return "[如何写每日任务](https://github.com/Sunny-117/github-action-auto-issues#readme)";
+}
